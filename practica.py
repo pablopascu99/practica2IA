a88d0b22490a36e932d301d596a496e56075c568
pablopascu99 <pablopascu99@gmail.com>
2021-11-19 18:17:37 +0100

    s

--- practica.py
+++ practica.py
@@ -1,4 +1,16 @@
 import pandas as pd
+from sklearn import preprocessing
+import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("./data/cardsNuevo.csv",sep = ',')
-print(df)
+df = pd.read_csv("../data/cardsNuevo.csv",sep = ',')
+newdf = df[["NUM_OP", "IMPORTE"]].dropna()
+
+kmeans = KMeans(n_clusters = 4, random_state = 0).fit(newdf)
+kmeans.labels_
+
+np.unique(kmeans.labels_, return_counts = True)
+
+plt.scatter(newdf["NUM_OP"], newdf["IMPORTE"], c= kmeans.labels_)
+plt.show()
